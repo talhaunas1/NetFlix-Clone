4a539c2296494d395e24fff89163f00035b635ba
talhaunas1 <mtalha4600@gmail.com>
2022-07-28 15:54:16 +0500

    installing packages

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "38b9741154027d8d5ed01184ed475f48";
+ 
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  };
+  //default export can be imported with different name e.g axios in Row.js
+  export default requests;
