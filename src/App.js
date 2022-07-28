7af2199b4ebbd3247aa611d4763a2f9f6ee97ce9
talhaunas1 <mtalha4600@gmail.com>
2022-07-28 16:21:40 +0500

    setting up axios for fetheching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Row from './component/Row';
+import requests from './request';
 function App() {
   return (
     <div className="App">
-      hello 
+      <Row fetchUrl={requests.fetchNetflixOriginals} />
+      <Row  fetchUrl={requests.fetchTrending}  />
     </div>
   );
 }
--- src/axiox.js
+++ src/axiox.js
@@ -1,3 +1,7 @@
 import axios from "axios";
 
-const 
+//baseUrl to make requests to the movie database
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3"
+})
+export default instance
