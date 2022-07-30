36d70c51220ca81b2f7b98535cd4ada172845206
talhaunas1 <mtalha4600@gmail.com>
2022-07-30 23:58:04 +0500

    setting up the size of poster

---
+++ src/component/Banner.js
@@ -0,0 +1,24 @@
+import React,{useEffect, useState} from 'react'
+
+function Banner() {
+
+    const [Movie, setMovie] = useState([])
+
+    useEffect(() => {
+      async funstion fetchData(){
+
+      }
+    fetchData()
+    }, [])
+    
+  return (
+    <header>
+         
+        {/* {title} */}
+        {/* {div(button) } */}
+        {/* {discripition} */}
+    </header>
+  )
+}
+
+export default Banner
